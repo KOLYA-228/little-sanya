f0b2989f00ed8d234e1e1fd537eed674a084ebf4
kolya <rawderggwplike@gmail.com>
2020-03-05 13:27:56 +0300

    Instal commit 3.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleApp65
             foreach (var item in result)
             {
                 Console.WriteLine(item);
+                Console.WriteLine("хлеб вкусный");
             }
         }
     }
